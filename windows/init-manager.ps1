ae55ccb9b96e258c794fe354070e52c637fc755a
ssbkang <skan070@gmail.com>
2021-03-23 20:37:55 +1300

    WIP

---
+++ windows/init-manager.ps1
@@ -0,0 +1,4 @@
+New-NetFirewallRule -DisplayName "Allow Swarm TCP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377, 7946 | Out-Null
+New-NetFirewallRule -DisplayName "Allow Swarm UDP" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789, 7946 | Out-Null
+
+docker swarm init --advertise-addr 10.0.1.11 --default-addr-pool 10.0.1.0/23
