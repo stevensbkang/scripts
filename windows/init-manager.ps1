424aac3f1e732d7d4077473cf82a191e87fa992a
Steven Kang <skan070@gmail.com>
2021-03-24 22:03:52 +1300

    Update init-manager.ps1

--- windows/init-manager.ps1
+++ windows/init-manager.ps1
@@ -5,9 +5,9 @@ New-NetFirewallRule -DisplayName 'Allow Swarm UDP' -Direction Inbound -Action Al
 ## Initialise Docker Swarm mode
 docker swarm init --advertise-addr 10.0.1.11 --default-addr-pool 10.0.1.0/23
 
-## Initial Configuration of SSH Server
+## Configuration of SSH Server
+Add-WindowsCapability -Online -Name (Get-WindowsCapability -Online -Name 'OpenSSH.Server*').Name
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 Get-NetFirewallRule -Name *ssh*
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-Add-WindowsCapability -Online -Name (Get-WindowsCapability -Online -Name 'OpenSSH.Server*').Name
