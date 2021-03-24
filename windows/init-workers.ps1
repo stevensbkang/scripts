385d10c815bb2d097d8d0c9772f4a3adf15ea2d8
Steven Kang <skan070@gmail.com>
2021-03-24 22:15:02 +1300

    Update init-workers.ps1

--- windows/init-workers.ps1
+++ windows/init-workers.ps1
@@ -4,4 +4,8 @@ param(
   $credentials
 )
 
-$token = ssh local_admin@
+New-NetFirewallRule -DisplayName "Allow Swarm TCP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377, 7946 | Out-Null
+New-NetFirewallRule -DisplayName "Allow Swarm UDP" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789, 7946 | Out-Null
+
+$token = ssh -o StrictHostKeyChecking=no local_admin@10.0.1.11 docker swarm join-token worker -q
+docker swarm join --token $token 10.0.1.11:2377
