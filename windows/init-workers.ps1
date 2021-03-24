174dbe0a287a59663c9c8232a127a3a1c25a6bdc
Steven Kang <skan070@gmail.com>
2021-03-24 22:32:43 +1300

    Update init-workers.ps1

--- windows/init-workers.ps1
+++ windows/init-workers.ps1
@@ -7,5 +7,8 @@ param(
 New-NetFirewallRule -DisplayName "Allow Swarm TCP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377, 7946 | Out-Null
 New-NetFirewallRule -DisplayName "Allow Swarm UDP" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789, 7946 | Out-Null
 
-$token = ssh -o StrictHostKeyChecking=no local_admin@10.0.1.11 docker swarm join-token worker -q
+curl https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -o plink.exe
+$finger_print = (((.\plink.exe -v -batch 10.0.1.11) 2>&1)[9] -split " ")[2]
+$token = .\plink.exe local_admin@10.0.1.11 -pw $credentials -hostkey $finger_print -batch 'docker swarm join-token worker -q' 2>&1 
+
 docker swarm join --token $token 10.0.1.11:2377
