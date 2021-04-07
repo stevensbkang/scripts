fc207a3003979a62340b1fae74cc6d1e0d5402bc
Steven Kang <skan070@gmail.com>
2021-04-07 14:50:28 +1200

    Update init.ps1

--- wvd/init.ps1
+++ wvd/init.ps1
@@ -4,13 +4,8 @@ param(
   $registration_token
 )
 
-while ((Get-Service RdAgent).Status -ne 'Running') { Start-Sleep -s 5 } 
-while ((Get-Service WindowsAzureTelemetryService).Status -ne 'Running') { Start-Sleep -s 5 } 
-while ((Get-Service WindowsAzureGuestAgent).Status -ne 'Running') { Start-Sleep -s 5 }
-
 curl https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv -o desktop_agent.msi
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i desktop_agent.msi", "/quiet", "/qn", "/norestart", "/passive", "REGISTRATIONTOKEN=$registration_token", "/l* C:\Users\AgentInstall.txt" -Wait -Passthru
 
 curl https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH -o desktop_agent_bootloader.msi
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i desktop_agent_bootloader.msi", "/quiet", "/qn", "/norestart", "/passive", "/l* C:\Users\AgentBootloaderInstall.txt" -Wait -Passthru
-
