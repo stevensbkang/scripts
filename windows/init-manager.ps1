561e4b67211e34eb6a1c05c3794f2e5715cbd0c6
Steven Kang <skan070@gmail.com>
2021-04-20 10:51:48 +1200

    Removed Portainer deployment

--- windows/init-manager.ps1
+++ windows/init-manager.ps1
@@ -1,8 +1,8 @@
-param(
-  [Parameter(Mandatory = $True)]
-  [string]
-  $portainer_image
-)
+# param(
+#   [Parameter(Mandatory = $True)]
+#   [string]
+#   $portainer_image
+# )
 
 ## Open Firewall for Docker Swarm mode Initialisation
 New-NetFirewallRule -DisplayName 'Allow Swarm TCP' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377, 7946 | Out-Null
@@ -19,14 +19,14 @@ Get-NetFirewallRule -Name *ssh*
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
 ## Install Portainer on Swarm
-docker volume create portainer_data
-docker service create `
-  --name portainer `
-  --publish 9000:9000 `
-  --publish 8000:8000 `
-  --replicas=1 `
-  --constraint 'node.role == manager' `
-  --mount 'type=volume,source=portainer_data,destination=C:/data' `
-  --mount 'type=npipe,source=\\.\pipe\docker_engine,destination=\\.\pipe\docker_engine' `
-  --mount 'type=bind,source=C:\ProgramData\docker\volumes,destination=C:\ProgramData\docker\volumes' `
-  $portainer_image
+# docker volume create portainer_data
+# docker service create `
+#   --name portainer `
+#   --publish 9000:9000 `
+#   --publish 8000:8000 `
+#   --replicas=1 `
+#   --constraint 'node.role == manager' `
+#   --mount 'type=volume,source=portainer_data,destination=C:/data' `
+#   --mount 'type=npipe,source=\\.\pipe\docker_engine,destination=\\.\pipe\docker_engine' `
+#   --mount 'type=bind,source=C:\ProgramData\docker\volumes,destination=C:\ProgramData\docker\volumes' `
+#   $portainer_image
