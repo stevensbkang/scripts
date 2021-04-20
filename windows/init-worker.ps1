6a406c0012c6d88ed1d8e7e3b2bf60edc5f7808f
Steven Kang <skan070@gmail.com>
2021-04-20 12:37:04 +1200

    Update init-worker.ps1

--- windows/init-worker.ps1
+++ windows/init-worker.ps1
@@ -8,9 +8,10 @@ New-NetFirewallRule -DisplayName "Allow Swarm TCP" -Direction Inbound -Action Al
 New-NetFirewallRule -DisplayName "Allow Swarm UDP" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789, 7946 | Out-Null
 
 curl https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -o plink.exe
-$finger_print = (((.\plink.exe -v -batch 10.0.1.11) 2>&1)[9] -split " ")[2]
+$finger_print_execution = '(((.\plink.exe -v -batch 10.0.1.11) 2>&1)[9] -split " ")[2]'
+$finger_print = Invoke-Expression $finger_print_execution
 
-$token = .\plink.exe local_admin@10.0.1.11 -pw $credentials -hostkey $finger_print -batch docker swarm join-token -q worker
-$token | Out-File token.txt
+$token_execution = '.\plink.exe local_admin@10.0.1.11 -pw $credentials -hostkey $finger_print -batch docker swarm join-token -q worker'
+$token = Invoke-Expression $token_execution
 
 docker swarm join --token $token 10.0.1.11:2377
