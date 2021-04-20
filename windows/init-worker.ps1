7196e3d902cdc1af1bc1d3bfe186cdf7c9d0178c
Steven Kang <skan070@gmail.com>
2021-04-20 20:27:45 +1200

    Update init-worker.ps1

--- windows/init-worker.ps1
+++ windows/init-worker.ps1
@@ -7,17 +7,9 @@ param(
 New-NetFirewallRule -DisplayName "Allow Swarm TCP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377, 7946 | Out-Null
 New-NetFirewallRule -DisplayName "Allow Swarm UDP" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789, 7946 | Out-Null
 
-## Configuration of SSH Server
-Add-WindowsCapability -Online -Name (Get-WindowsCapability -Online -Name 'OpenSSH.Server*').Name
-Start-Service sshd
-Set-Service -Name sshd -StartupType 'Automatic'
-Get-NetFirewallRule -Name *ssh*
-New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-
 curl https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -o plink.exe
 
 $token_execution = "cmd.exe /c echo y | ./plink.exe 10.0.1.11 -P 22 -l local_admin -pw '$credentials' docker swarm join-token -q worker"
-Invoke-Expression $token_execution
 $token = Invoke-Expression $token_execution
 
 Write-Host "The Token is $token"
