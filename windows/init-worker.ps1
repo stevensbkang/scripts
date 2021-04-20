ec1e404954adf855ba3bd63725f5c7d5b524fc43
Steven Kang <skan070@gmail.com>
2021-04-20 22:03:37 +1200

    Update init-worker.ps1

--- windows/init-worker.ps1
+++ windows/init-worker.ps1
@@ -7,7 +7,9 @@ param(
 New-NetFirewallRule -DisplayName "Allow Swarm TCP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377, 7946 | Out-Null
 New-NetFirewallRule -DisplayName "Allow Swarm UDP" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789, 7946 | Out-Null
 
-curl https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -o plink.exe
+$plink_execution = "curl https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -o plink.exe"
+Invoke-Expression $plink_execution
+
 $finger_print_execution = "((.\plink.exe -batch 10.0.1.11) 2>&1)"
 $finger_print = (((Invoke-Expression $finger_print_execution) -match "^ssh-") -split " ")[2]
 
