09d16a3e2de4686b99a33f0cea40b993a7d9ca1d
Steven Kang <skan070@gmail.com>
2021-04-21 20:08:39 +1200

    Update init-manager.ps1

--- windows/init-manager.ps1
+++ windows/init-manager.ps1
@@ -24,28 +24,7 @@ New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled Tru
 
 ## Install Portainer on Swarm
 if ( $portainer_environment_is_agent ) {
-  docker volume create portainer_data
-  docker network create -d overlay portainer_agent_network
-  
-  docker service create `
-    --name portainer_agent `
-    --mode=global `
-    --network=portainer_agent_network `
-    --constraint 'node.platform.os == windows' `
-    --mount 'type=npipe,source=\\.\pipe\docker_engine,destination=\\.\pipe\docker_engine' `
-    --mount 'type=bind,source=C:\ProgramData\docker\volumes,destination=C:\ProgramData\docker\volumes' `
-    $portainer_agent_image
-  
-  docker service create `
-    --name portainer `
-    --publish 9000:9000 `
-    --publish 8000:8000 `
-    --replicas=1 `
-    --network=portainer_agent_network `
-    --constraint 'node.role == manager' `
-    --constraint 'node.platform.os == windows' `
-    --mount 'type=volume,source=portainer_data,destination=C:/data' `
-    $portainer_image -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
+
     
 } elseif ( $portainer_environment_is_edge ) {
 
