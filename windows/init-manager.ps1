319f54a37847e3fc3148cbcaef745ba27d7deef8
Steven Kang <skan070@gmail.com>
2021-04-21 20:06:57 +1200

    Update init-manager.ps1

--- windows/init-manager.ps1
+++ windows/init-manager.ps1
@@ -5,6 +5,9 @@ param(
   $portainer_environment_is_edge
 )
 
+## Fix for Docker Swarm Network
+wusa /install /kb:4580390
+
 ## Open Firewall for Docker Swarm mode Initialisation
 New-NetFirewallRule -DisplayName 'Allow Swarm TCP' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377, 7946 | Out-Null
 New-NetFirewallRule -DisplayName 'Allow Swarm UDP' -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789, 7946 | Out-Null
@@ -21,28 +24,29 @@ New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled Tru
 
 ## Install Portainer on Swarm
 if ( $portainer_environment_is_agent ) {
-  #   docker volume create portainer_data
-#   docker network create -d overlay portainer_agent_network
+  docker volume create portainer_data
+  docker network create -d overlay portainer_agent_network
   
-#   docker service create `
-#     --name portainer_agent `
-#     --mode=global `
-#     --network=portainer_agent_network `
-#     --constraint 'node.platform.os == windows' `
-#     --mount 'type=npipe,source=\\.\pipe\docker_engine,destination=\\.\pipe\docker_engine' `
-#     --mount 'type=bind,source=C:\ProgramData\docker\volumes,destination=C:\ProgramData\docker\volumes' `
-#     $portainer_agent_image
+  docker service create `
+    --name portainer_agent `
+    --mode=global `
+    --network=portainer_agent_network `
+    --constraint 'node.platform.os == windows' `
+    --mount 'type=npipe,source=\\.\pipe\docker_engine,destination=\\.\pipe\docker_engine' `
+    --mount 'type=bind,source=C:\ProgramData\docker\volumes,destination=C:\ProgramData\docker\volumes' `
+    $portainer_agent_image
   
-#   docker service create `
-#     --name portainer `
-#     --publish 9000:9000 `
-#     --publish 8000:8000 `
-#     --replicas=1 `
-#     --network=portainer_agent_network `
-#     --constraint 'node.role == manager' `
-#     --constraint 'node.platform.os == windows' `
-#     --mount 'type=volume,source=portainer_data,destination=C:/data' `
-#     $portainer_image -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
+  docker service create `
+    --name portainer `
+    --publish 9000:9000 `
+    --publish 8000:8000 `
+    --replicas=1 `
+    --network=portainer_agent_network `
+    --constraint 'node.role == manager' `
+    --constraint 'node.platform.os == windows' `
+    --mount 'type=volume,source=portainer_data,destination=C:/data' `
+    $portainer_image -H "tcp://tasks.portainer_agent:9001" --tlsskipverify
+    
 } elseif ( $portainer_environment_is_edge ) {
 
 }
