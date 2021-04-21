1aa52ab80f836059bd7fd694caac43fd053adde5
Steven Kang <skan070@gmail.com>
2021-04-21 21:34:39 +1200

    Create init-standalone.sh

---
+++ linux/init-standalone.sh
@@ -0,0 +1,21 @@
+portainer_image=$1
+
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+sudo apt-get -y update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker local_admin
+
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9000:9000 \
+  --name=portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data $portainer_image
