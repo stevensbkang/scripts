201110bfac9f50b1ec7be49377a6f43d43b7cb14
Steven Kang <skan070@gmail.com>
2021-04-21 21:41:26 +1200

    Update init-standalone.ps1

--- windows/init-standalone.ps1
+++ windows/init-standalone.ps1
@@ -13,4 +13,4 @@ docker run -d -p 9000:9000 `
   --mount 'type=npipe,source=\\.\pipe\docker_engine,destination=\\.\pipe\docker_engine' `
   --mount 'type=bind,source=C:\ProgramData\docker\volumes,destination=C:\ProgramData\docker\volumes' `
   --mount 'type=volume,source=portainer_data,destination=C:/data' `
-  $portainer_image  
+  $portainer_image
