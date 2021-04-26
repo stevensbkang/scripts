b739dea017a33619e57872093449656f1682b747
Steven Kang <skan070@gmail.com>
2021-04-26 21:07:13 +1200

    Update init-standalone.ps1

--- windows/init-standalone.ps1
+++ windows/init-standalone.ps1
@@ -7,9 +7,7 @@ param(
 # Get-WindowsUpdate -Install -AutoReboot:$false -ForceDownload -Confirm:$false
 
 docker volume create portainer_data
-docker run --name portainer -d `
-  -p 9000:9000 `  
-  --restart always `
+docker run --name portainer -d -p 9000:9000 --restart always `
   --mount 'type=npipe,source=\\.\pipe\docker_engine,destination=\\.\pipe\docker_engine' `
   --mount 'type=bind,source=C:\ProgramData\docker\volumes,destination=C:\ProgramData\docker\volumes' `
   --mount 'type=volume,source=portainer_data,destination=C:/data' `
