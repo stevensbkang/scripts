08fad5be712907c20a419d54687514059fc09ee8
Steven Kang <skan070@gmail.com>
2021-04-27 20:03:02 +1200

    Update init-standalone.sh

--- linux/init-standalone.sh
+++ linux/init-standalone.sh
@@ -13,9 +13,13 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker local_admin
 
+echo -n '1nTegr@tion' > /tmp/portainer_password
+
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 \
   --name=portainer \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data $portainer_image
+  -v /tmp/portainer_password:/tmp/portainer_password \
+  -v portainer_data:/data $portainer_image \
+  --admin-password-file /tmp/portainer_password
