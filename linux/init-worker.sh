e8a4b64e2344fa5380a480086cd0deda542a8f5d
Steven Kang <skan070@gmail.com>
2021-05-03 19:32:32 +1200

    Update init-worker.sh

--- linux/init-worker.sh
+++ linux/init-worker.sh
@@ -4,7 +4,7 @@ portainer_environment_is_edge=$3
 
 if [ "${portainer_environment_is_agent}" ]; then
   echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials hostname
-  token=$(echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials docker swarm join-token -q worker)
+  token=$(echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials sudo docker swarm join-token -q worker)
   sudo docker swarm join --token $token 10.0.1.11:2377
 elif [ "${portainer_environment_is_edge}" ]; then
   echo "Edge to be configured"
