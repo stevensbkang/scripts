f50e36de1051d90ddd85352358f6fb77964b2d51
Steven Kang <skan070@gmail.com>
2021-05-03 19:06:40 +1200

    Update init-worker.sh

--- linux/init-worker.sh
+++ linux/init-worker.sh
@@ -14,7 +14,7 @@ credentials=$1
 # sudo usermod -aG docker local_admin
 
 ## Install Putty tools for plink
-sudo apt-get install -y putty-tools
+# sudo apt-get install -y putty-tools
 
 echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials hostname
 token=$(echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials docker swarm join-token -q worker)
