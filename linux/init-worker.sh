02584dea8dd4c5d68e0385143e529eff39c5412c
Steven Kang <skan070@gmail.com>
2021-05-03 20:53:35 +1200

    Update init-worker.sh

--- linux/init-worker.sh
+++ linux/init-worker.sh
@@ -8,5 +8,13 @@ if [ "${portainer_environment_is_agent}" ]; then
   token=$(echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials sudo docker swarm join-token -q worker)
   sudo docker swarm join --token $token 10.0.1.11:2377
 elif [ "${portainer_environment_is_edge}" ]; then
-  echo "Edge to be configured"
+  # portainer_jwt=$(curl -d "{\"username\":\"admin\", \"password\":\"${portainer_admin_password}\"}" -X POST http://20.193.66.178:9000/api/auth | jq .jwt | sed 's/\"//g')
+  
+  portainer_jwt=$(http POST http://20.193.66.178:9000/api/auth Username="admin" Password="${portainer_admin_password}" | jq .jwt | sed 's/\"//g')
+    
+  edge_key=$(http --form POST http://20.193.66.178:9000/api/endpoints \
+    "Authorization: Bearer ${portainer_jwt}" \
+    Name="edge-agent" EndpointCreationType=4 | jq .EdgeKey | sed 's/\"//g')
+      
+  
 fi
