970275da2dea39afe81a287230affe10c4868764
Steven Kang <skan070@gmail.com>
2021-05-03 22:02:37 +1200

    Update init-worker.sh

--- linux/init-worker.sh
+++ linux/init-worker.sh
@@ -9,14 +9,14 @@ echo $portainer_environment_is_agent
 echo "Edge: "
 echo $portainer_environment_is_edge
 
-if [ "${portainer_environment_is_agent}" ]; then
+if [ "${portainer_environment_is_agent}" == true ]; then
   ## Leverage SSH to grab the Swarm join token
   echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials hostname
   token=$(echo y | plink 10.0.1.11 -P 22 -l local_admin -pw $credentials sudo docker swarm join-token -q worker)
   
   ## Join Swarm manager with the token above
   sudo docker swarm join --token $token 10.0.1.11:2377
-elif [ "${portainer_environment_is_edge}" ]; then
+elif [ "${portainer_environment_is_edge}" == true ]; then
   ## Get the current IP address
   ip_address=$(hostname -I | cut -d ' ' -f 1)
   
