faa01b18793f5525a906fecbbdab525b612365a3
Steven Kang <skan070@gmail.com>
2021-05-03 13:39:47 +1200

    Update init.ps1

--- wvd/init.ps1
+++ wvd/init.ps1
@@ -1,7 +1,7 @@
 param(
   [Parameter(Mandatory = $True)]
-  [string]
-  $registration_token
+  [string] $registration_token,
+  [string] $vhd_location
 )
 
 curl https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv -o desktop_agent.msi
@@ -9,3 +9,12 @@ Start-Process -FilePath "msiexec.exe" -ArgumentList "/i desktop_agent.msi", "/qu
 
 curl https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH -o desktop_agent_bootloader.msi
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i desktop_agent_bootloader.msi", "/quiet", "/qn", "/norestart", "/passive", "/l* C:\Users\AgentBootloaderInstall.txt" -Wait -Passthru
+
+## Install FSLogix
+curl https://go.microsoft.com/fwlink/?linkid=2084562 -o FSLogix.zip
+Expand-Archive .\FSLogix.zip
+Start-Process -Wait -FilePath ".\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install", "/quiet"
+
+New-Item –Path "HKLM:\SOFTWARE\FSLogix" –Name Profiles
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value "1" -PropertyType "DWORD"
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value "\\sapocdemocps.file.core.windows.net\fslogix" -PropertyType "MultiString"
